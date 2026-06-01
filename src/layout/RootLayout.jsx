453ad37c9a45f3ac73845a539350794265200447
hxx-dev <red05077@gmail.com>
2026-06-01 19:50:00 +0900

    :bug:Fix:레이아웃 코드 수정 및 폰트 오타 해결

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/header/Header";
-import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header.jsx";
+// import Footer from "../components/footer/Footer";
 
 export default function RootLayout() {
   return (
@@ -9,7 +9,7 @@ export default function RootLayout() {
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
