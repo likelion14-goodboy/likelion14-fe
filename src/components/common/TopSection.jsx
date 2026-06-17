cc014fb59b053f9822958e74b809e575f27b56a1
jangkyoungmoo <jangkyoungmoo@naver.com>
2026-06-18 00:24:23 +0900

    ✨Feat: TopSection 컴포넌트 추가 구현  및 Home페이지 구현

--- src/components/common/TopSection.jsx
+++ src/components/common/TopSection.jsx
@@ -2,24 +2,40 @@ import styled from "styled-components";
 
 const TopSectionContainer = styled.div`
 display: flex;
+flex-direction: column;
 background: #4A25ED;
 height: 1315px;
-flex-direction: column;
-
-`;
-
-const GraphicImage = styled.img`
+align-items: center;
+justify-content: center;
 `;
 
 const Title = styled.div`
+text-align: center;
+.sub-title {
+color: white;
+font-size: 90px;
+font-weight: 600;
+display: block;
+}
+
+.main-title {
+color: #FF8BD4;
+font-size: 120px;
+font-weight: 600;
+}
+
+.point-text {
+color: white;
+font-size: 120px;
+font-weight: 600;
+}
 `;
 
-
-export default function TopSection({title, imgSrc}) {
+export default function TopSection({children, imageContent}) {
     return (
         <TopSectionContainer>
-            <title>{title}</title>
-            <GraphicImage src={imgSrc} />
+            <Title>{children}</Title>
+            <div>{imageContent}</div>
         </TopSectionContainer>
     )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import TopSection from "../components/common/TopSection";
+import HomeSwitchImg from "../assets/images/button_image.svg";
+import HomeWheelImg from "../assets/images/wheel_image.svg";
+
+const HomeContainer = styled.div`
+`;
+
+const HomeImageContainer = styled.div`
+margin-top: 32px;
+position: relative;
+`;
+
+const Switch = styled.img`
+width: 591px;
+height: 624px;
+`;
+
+const Wheel = styled.img`
+position: absolute;
+top: 50px;
+right: -98px;
+width: 230px;
+height: 240px;
+`;
+
+export default function Home() {
+    return (
+        <HomeContainer>
+            <TopSection
+            imageContent={
+                <HomeImageContainer>
+                    <Switch src={HomeSwitchImg} />
+                    <Wheel src={HomeWheelImg} />
+                </HomeImageContainer>
+             }
+            >
+             <span className="sub-title">월요일에 만나는</span>
+             <span className="main-title">프론트엔드 </span>
+             <span className="point-text">한 스푼.</span>
+            </TopSection>
+        </HomeContainer>
+    );
+}
